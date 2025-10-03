7802a515c3f82c0869a8af291154b951d1a17fbd
maanubaniwal <maanubaniwal@gmail.com>
2025-10-03 10:28:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ if (isIndexPage) {
         if (playBtn) {
             playBtn.addEventListener('click', () => {
                 if (!song) {
-                    song = new Audio('breathless.mp3'); // Make sure your song name is correct here
+                   song = new Audio('breathless.mp3');// Make sure your song name is correct here
                     song.loop = true;
                 }
                 
@@ -150,4 +150,5 @@ if (isIndexPage) {
     
     animate();
 }
+
 
